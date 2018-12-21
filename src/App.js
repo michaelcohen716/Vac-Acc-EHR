169ff35c27826c1d8b4148f5b13480a789025859
rene <renegmed@yahoo.com>
2018-12-20 23:02:16 -0500

    start of frontend dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './img/Vac-Acc-logo.png';
-import './App.css';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <Route exact path="/" render={ () => (
+            <div>
+              Main Page
+            </div>    
+         )} />
+
+         <Route exact path="/Attestation" render={ () => (
+            <div>
+              Attestation Page
+            </div>    
+         )} />
+
       </div>
     );
   }
