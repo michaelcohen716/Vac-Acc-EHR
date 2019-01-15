c9a39bc81d0e7d7f5935cfdc355170a667d9050e
Michael Cohen <michaelcohen716@gmail.com>
2019-01-14 23:10:17 -0500

    set up react native project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
- 
